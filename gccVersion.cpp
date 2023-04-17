bb2f24f05729ce553baea2fc807d111e562b7d66
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2023-04-18 01:52:34 +0530

    Added a CPP FIle to check the current GCC Version

---
+++ gccVersion.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "GCC version: " << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__ << "\n";
+    return 0;
+}
+
